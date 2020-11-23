c9498ae8d4768e762b563ecdabda08c11489e9ea
RemyRM <remy_rm@hotmail.nl>
2020-11-23 11:34:36 +0100

    Added a class that contains the Domain tile which exists of a left and right tile. Tiles get their colour and type assigned based on the hex value in the map data file. Added Domain class that will contain all the maps that can be loaded into it. Added a Method to load in a pointer at given address and return it as little Endian. Constructor of Domain class now follows the first pointer in the map data file to the name of the domain, and converts the hex value of that name into ASCII text

---
+++ DigimonWorld2MapVisualizer/DigimonWorld2MapVisualizer/DomainFloorLayout.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigimonWorld2MapVisualizer
+{
+    class DomainFloorLayout
+    {
+        public List<DomainTile> domainTiles = new List<DomainTile>();
+    }
+}
--- DigimonWorld2MapVisualizer/DigimonWorld2MapVisualizer/Program.cs
+++ DigimonWorld2MapVisualizer/DigimonWorld2MapVisualizer/Program.cs
@@ -4,9 +4,40 @@ namespace DigimonWorld2MapVisualizer
 {
     class Program
     {
+        private static readonly string mapFileName = "DUNG7000.BIN"; // 4000 SCSI, 4900 has coloured floors, 7000 is DVD Domain which has 1 floor only
+        public static bool ShowOriginalValueInMapTile = true;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting Digimon World 2 domain visualizer\n");
+                
+            InitConsole();
+            PrintIndex();
+
+            Domain dom = new Domain(mapFileName);
+
+            Console.WriteLine("\n\nEnd of visualisation, press any key to exit.");
+            Console.ReadLine();
+        }
+
+        static void InitConsole()
+        {
+            Console.SetBufferSize(Console.BufferWidth, 32766);
+            Console.WindowWidth = Console.LargestWindowWidth;
+            Console.WindowHeight = Console.LargestWindowHeight;
+        }
+
+        static void PrintIndex()
+        {
+            Console.WriteLine($"Colour index: " +
+                              $"\nEmpty    - Black" +
+                              $"\nRoom     - White" +
+                              $"\nCorridor - Grey" +
+                              $"\nWater    - Dark Blue" +
+                              $"\nFire     - Dark Red" +
+                              $"\nNature   - Dark Green" +
+                              $"\nMachine  - Dark Yellow" +
+                              $"\nDark     - Dark Magenta");
         }
     }
 }

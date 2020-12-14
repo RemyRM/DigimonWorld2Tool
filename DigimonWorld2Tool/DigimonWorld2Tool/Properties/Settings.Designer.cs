160d5a90b258e13fef880eff982433bb9257aa1b
RemyRM <remy_rm@hotmail.nl>
2020-12-14 18:13:12 +0100

    Added the ability to change where the map data gets pulled from

--- DigimonWorld2Tool/DigimonWorld2Tool/Properties/Settings.Designer.cs
+++ DigimonWorld2Tool/DigimonWorld2Tool/Properties/Settings.Designer.cs
@@ -82,5 +82,17 @@ namespace DigimonWorld2Tool.Properties {
                 this["ShowLogs"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string MapDataFolder {
+            get {
+                return ((string)(this["MapDataFolder"]));
+            }
+            set {
+                this["MapDataFolder"] = value;
+            }
+        }
     }
 }
